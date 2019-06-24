fd494b9f5b087e26846943493248802bb15e4108
phankerson4 <phankerson4@gmail.com>
2019-06-24 14:25:08 -0400

    Goal 1 - Books

---
+++ book/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.urls import reverse # Used to generate URLs by reversing the URL patterns
+# Create your models here.
+
+
+class Book(models.Model):
+    """Model representing a book (but not a specific copy of a book)."""
+    title = models.CharField(max_length=200)
+
+    # Foreign Key used because book can only have one author, but authors can have multiple books
+    # Author as a string rather than object because it hasn't been declared yet in the file
+    #author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    
+    description = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
+    isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    publication_date = models.DateField(blank=True, null=True)
+    
+    
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.title
+    
+    def get_absolute_url(self):
+        """Returns the url to access a detail record for this book."""
+        return reverse('book-detail', args=[str(self.id)])
