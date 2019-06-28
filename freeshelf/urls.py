fa37ce9d910ba07d436c1dcc1583176b2297dc18
phankerson4 <phankerson4@gmail.com>
2019-06-28 15:04:23 -0400

    Added Heroku to Settings.py

--- freeshelf/urls.py
+++ freeshelf/urls.py
@@ -41,9 +41,7 @@ urlpatterns += [
 
 #Add Django site authentication urls (for login, logout, password management)
 #urlpatterns += [
-    #path('accounts/', include('django.contrib.auth.urls')),
-#]
-
+    #path('accounts/', include('django.contrib.auth.urls')),]
 
 
 
